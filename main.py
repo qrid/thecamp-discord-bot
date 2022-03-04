8c14090aa8d633345dab89ff11218f60dba01925
qrid <rbwls5567@naver.com>
2022-03-04 23:21:06 +0900

    image converting bug fixing(WIP) : updated(0.0.230)

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def hello(ctx):
 
 @bot.command(aliases=['버전'])
 async def version(ctx):
-    await ctx.send("version : 0.0.29")
+    await ctx.send("version : 0.0.30")
 
 
 @bot.command(aliases=['전역', '언제옴', '디데이', 'dday', 'd-day', '달성률', '몇퍼'])
@@ -128,7 +128,7 @@ async def send_button(ctx):
         print(image, type(image))
 
     data = {"sender": sender, "subject": subject, "content": content}
-    r = requests.post(url + "letter/", data=data, files={"image": image})
+    r = requests.post(url + "letter/", data=data, files={"image": image.decode()})
     print(r.status_code)
 
     await asyncio.sleep(3)
