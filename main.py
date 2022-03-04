983aff7cc4d02b37457f8192ffb373a5984c47b2
qrid <rbwls5567@naver.com>
2022-03-05 01:49:38 +0900

    image converting bug fixing(WIP) : updated(0.1.2)

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def hello(ctx):
 
 @bot.command(aliases=['버전'])
 async def version(ctx):
-    await ctx.send("version : 0.1.1")
+    await ctx.send("version : 0.1.2")
 
 
 @bot.command(aliases=['전역', '언제옴', '디데이', 'dday', 'd-day', '달성률', '몇퍼'])
@@ -132,7 +132,10 @@ async def send_button(ctx):
         print(image_url, image_ext)
 
         req = requests.get(image_url)
-        image = Image.open(io.BytesIO(req.content))
+        image_pil = Image.open(io.BytesIO(req.content))
+        image_bytes = io.BytesIO()
+        image_pil.save(image_bytes, image_ext)
+        image = image_bytes
 
     data = {"sender": sender, "subject": subject, "content": content}
     r = requests.post('https://httpbin.org/post', data=data, files={'image': image})
