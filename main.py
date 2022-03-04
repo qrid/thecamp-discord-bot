c7763dc3a90e04f3f498d3a707f9119609c8c8c6
qrid <rbwls5567@naver.com>
2022-03-04 23:02:01 +0900

    image converting bug fixing(WIP) : updated(0.0.27)

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def hello(ctx):
 
 @bot.command(aliases=['버전'])
 async def version(ctx):
-    await ctx.send("version : 0.0.26")
+    await ctx.send("version : 0.0.27")
 
 
 @bot.command(aliases=['전역', '언제옴', '디데이', 'dday', 'd-day', '달성률', '몇퍼'])
@@ -124,7 +124,7 @@ async def send_button(ctx):
 
     image = None
     if image_url != discord.Embed.Empty:
-        image = requests.get(image_url)
+        image = requests.get(image_url).content
 
     data = {"sender": sender, "subject": subject, "content": content}
     r = requests.post(url + "letter/", data=data, files={"image": image})
