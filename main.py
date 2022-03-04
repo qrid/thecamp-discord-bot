4ff8fbbf99d7721f0d79609ae429c7b61d3de759
qrid <rbwls5567@naver.com>
2022-03-04 21:11:18 +0900

    bug fixed : updated(0.0.11)

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def hello(ctx):
 
 @bot.command(aliases=['버전'])
 async def version(ctx):
-    await ctx.send("version : 0.0.10")
+    await ctx.send("version : 0.0.11")
 
 
 @bot.command(aliases=['전역', '언제옴', '디데이', 'dday', 'd-day', '달성률', '몇퍼'])
@@ -126,7 +126,7 @@ async def send_button(ctx):
     image = None
     if image_url != discord.Embed.Empty:
         image_url = Request(image_url, headers={'User-Agent': 'Mozilla/5.0'})
-        image = urlopen(image_url).open()
+        image = urlopen(image_url).read()
 
     data = {"sender": sender, "subject": subject, "content": content, "image": image}
     r = requests.post(url + "letter/", json=data)
